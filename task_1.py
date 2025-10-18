277cee55e461631737f976d636791df16d7d9ed7
ArtemM <artemmorhunenko241@gmail.com>
2025-10-18 17:06:31 +0300

    Внесено зміни в перше завдання, яке усуває помилку

--- task_1.py
+++ task_1.py
@@ -7,12 +7,12 @@ def get_days_from_today(date):
     try: #У разі неправильного вводу формату обробляє помилку
         one_date = datetime.strptime(date, '%Y-%m-%d')
         current_date = datetime.today()
-        difference = (one_date - current_date).days
+        difference = (current_date - one_date).days
         print(f'Різниця між {one_date.date()} і сьогодні: {difference}')
         return difference
     except ValueError:
         print('Помилка неправильного формату дати. Використовуйте формат "YYYY-MM-DD"')
         return None
 
-date = '2024-10-10'
+date = '2024-10-18'
 get_days_from_today(date)
